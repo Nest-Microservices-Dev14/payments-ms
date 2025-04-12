ffdd60aa0e5fdd3a4f34e202b9dfaeb20f030935
castillo <fabianboc960@hotmail.com>
2025-04-12 14:30:09 -0500

    feat: :sparkles: Agregar configuración de entorno y creación de recursos de pago.
    Se incluye validación de variables de entorno y nuevas dependencias en package.json

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config/envs';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+
+  const logger = new Logger('Main - Payments');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envs.port);
+  logger.log(`Client Microservice Running on port ${ envs.port }`);
+
 }
 bootstrap();
