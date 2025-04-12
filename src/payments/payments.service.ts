c7a75a6b3fd784a13e83295815f32ed1819abaf3
castillo <fabianboc960@hotmail.com>
2025-04-12 16:02:51 -0500

    feat: :sparkles: Configuración de prueba de Stripe y enrutamiento a la creación de sesión de payment
    Se añade la clave STRIPE_SECRET en .env.template, se actualizan las dependencias en package.json y package-lock.json, y se implementa la lógica para crear sesiones de pago en PaymentsService.

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { envs } from 'src/config/envs';
+import Stripe from 'stripe';
 
 @Injectable()
-export class PaymentsService {}
+export class PaymentsService {
+    private readonly stripe = new Stripe(envs.stripeSecret);
+
+    async createPaymentSession(){
+        return await this.stripe.checkout.sessions.create({
+            payment_intent_data: {
+                metadata: {}
+            },
+
+            line_items:[
+                {
+                    price_data: {
+                        currency: 'usd',
+                        product_data: {
+                            name: 'T-Shirt'
+                        },
+                        unit_amount: 2000
+                    },
+                    quantity: 2
+                }
+            ],
+            mode: 'payment',
+            success_url: 'http://localhost:3003/payments/success',
+            cancel_url: 'http://localhost:3003/payments/cancel',
+        })
+    }
+}
