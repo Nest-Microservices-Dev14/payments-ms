a38ab02d5ee3d6fe42551360edb79767e6329141
castillo <fabianboc960@hotmail.com>
2025-04-12 22:12:08 -0500

    feat: :sparkles: Implementación de validación y mejora en la creación de sesiones de pago
    Se añaden las dependencias 'class-transformer' y 'class-validator' en package.json. Se implementa la validación de datos en PaymentsController y PaymentsService, y se crea el DTO PaymentSessionDto para estructurar la información de la sesión de pago.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config/envs';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 
   const logger = new Logger('Main - Payments');
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true
+    })
+  );
+
   await app.listen(envs.port);
   logger.log(`Client Microservice Running on port ${ envs.port }`);
 
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,32 +1,58 @@
 import { Injectable } from '@nestjs/common';
 import { envs } from 'src/config/envs';
 import Stripe from 'stripe';
+import { PaymentSessionDto } from './dto/payment-session.dto';
+import { Request, Response } from 'express';
 
 @Injectable()
 export class PaymentsService {
     private readonly stripe = new Stripe(envs.stripeSecret);
 
-    async createPaymentSession(){
+    async createPaymentSession(paymentSessionDto: PaymentSessionDto){
+        const { currency, items } = paymentSessionDto;
+
+        const lineItems = items.map( item => {
+            return {
+                price_data: {
+                    currency,
+                    product_data: {
+                        name: item.name
+                    },
+                    unit_amount: Math.round( item.price * 100 )
+                },
+                quantity: item.quantity
+            }
+        });
+
         return await this.stripe.checkout.sessions.create({
             payment_intent_data: {
                 metadata: {}
             },
 
-            line_items:[
-                {
-                    price_data: {
-                        currency: 'usd',
-                        product_data: {
-                            name: 'T-Shirt'
-                        },
-                        unit_amount: 2000
-                    },
-                    quantity: 2
-                }
-            ],
+            line_items: lineItems,
             mode: 'payment',
             success_url: 'http://localhost:3003/payments/success',
             cancel_url: 'http://localhost:3003/payments/cancel',
-        })
+        });
+    }
+
+    async stripeWebhook( req: Request, res: Response){
+        const sig = req.headers['stripe-signature'] as string;
+        let event: Stripe.Event;
+        const endpointSecret = "whsec_86309b22d9d0fa43dc8a0ec7bf3b279f04e6cd241316c9a6c002b7d70e2307a7";
+
+        try {
+            event = this.stripe.webhooks.constructEvent(
+                req['rawBody'],
+                sig,
+                endpointSecret
+            );
+        } catch (error) {
+            console.log('error', error.message);
+            res.status(400).send(`Webhook Error: ${error.message}`);
+            return;
+        }
+        console.log({ event })
+        return res.status(200).json({ sig });
     }
 }
